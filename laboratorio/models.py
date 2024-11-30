a894ad352753e480fc0092300bede4e2f970dfa7
Jorge Cordova <55065873+jcordovaj@users.noreply.github.com>
2024-11-29 22:47:22 -0300

    update agregar campos para migracion con nombre

--- laboratorio/models.py
+++ laboratorio/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Laboratorio(models.Model):
     id = models.AutoField(primary_key=True)
     nom_lab = models.CharField(max_length=255, verbose_name='nombre')
+    alias_lab = models.CharField(max_length=255, verbose_name='alias')
     city_lab = models.CharField(max_length=255, verbose_name='ciudad', default=None)
     pais_lab = models.CharField(max_length=255, verbose_name='pais', default=None)
     creado_lab = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Creación')
@@ -17,12 +18,10 @@ class Laboratorio(models.Model):
     def __str__(self):
         return self.nom_lab
 
-
 class DirectorGeneral(models.Model):
     
     id = models.AutoField(primary_key=True)
     nom_dire = models.CharField(max_length=255, verbose_name='nombre')
-    #lab_dire = models.OneToOneField('Laboratorio', on_delete=models.CASCADE)
     lab_dire = models.OneToOneField(Laboratorio, on_delete=models.CASCADE, related_name='director')
     especialidad = models.CharField(max_length=255, verbose_name='especialidad', default=None)
     creado_dire = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Creación')
@@ -36,11 +35,6 @@ class DirectorGeneral(models.Model):
     def __str__(self):
         return self.nom_dire
 
-#anios_choices = []
-
-#for anio in range(2015, (datetime.datetime.now().year+1)):
-#    anios_choices.append((anio, anio))
-
 def anio_actual():
         return int(datetime.date.today().year)
 
@@ -52,6 +46,7 @@ class Producto(models.Model):
     nom_prod = models.CharField(max_length=255, verbose_name='Producto')
     lab_prod = models.ForeignKey('Laboratorio', on_delete=models.SET_NULL, blank=True, null=True)
     f_fabricacion = models.IntegerField(choices=anios_choices, default=anio_actual, verbose_name='F Fabricación')
+    f_expiracion = models.IntegerField(choices=anios_choices, default=anio_actual, verbose_name='F Expiración') 
     p_costo = models.DecimalField(null=False, max_digits=12, decimal_places=2, verbose_name='P Costo')
     p_venta = models.DecimalField(null=False, max_digits=12, decimal_places=2, verbose_name='P Venta')
     creado_prod = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Creación')
